a187be9196a06d032e15f58842704063bd87546f
LuciferX12Z <khantkoaung1200@gmail.com>
2022-12-01 23:26:32 +0630

    creating library

--- src/Library/Library.jsx
+++ src/Library/Library.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+// import React from 'react'
 
-export const Library = () => {
-    return (
-        <div>
-            Library
+// export const Library = () => {
+//     return (
+//         <div>
+//             Library
+//         </div>
+//     )
+// }
+
+import { Col, Row } from "antd";
+import React, { useContext } from "react";
+import { Button } from "antd";
+import { CardComponent } from "../Exporter/Exporter";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { UserContext } from "../contexts/UserContext";
+import { Link } from "react-router-dom";
+
+const url = process.env.REACT_APP_BACKEND_URL;
+export const Library = (props) => {
+  const [courses, setCourses] = useState([]);
+  const [isLoggedIn] = useContext(UserContext);
+
+  // set "withCredentials : true" to send cookies with every request
+  useEffect(() => {
+    axios
+      .get(`${url}/getcourse/`, {
+        withCredentials: true,
+      })
+      .then((res) => setCourses(res.data));
+  }, []);
+  return (
+    <>
+      {isLoggedIn && (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row-reverse",
+            margin: "1rem 1rem 0 0",
+          }}
+        >
+          <Link
+            to={{
+              pathname: "/addCourse",
+              state: { ...props?.location?.state?.course, isEdit: false },
+            }}
+          >
+            <Button type="primary">Add New Book</Button>
+          </Link>
         </div>
-    )
-}
+      )}
+      <Row wrap justify>
+        {courses?.course?.map((item) => (
+          <Col
+            key={item._id}
+            lg={6}
+            md={12}
+            sm={12}
+            xs={24}
+            style={{ display: "flex", justifyContent: "center" }}
+          >
+            {" "}
+            <CardComponent course={item} history={props?.history} />{" "}
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
